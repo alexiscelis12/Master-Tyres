2b8592b9e91aa20ea172eebb009fc183897441b8
Alexis Celis <celis.alexis.1hm@gmail.com>
2025-06-24 19:43:05 -0600

    Creacion del proyecto

---
+++ src/main/java/com/mastertyres/master_tyres/MasterTyresApplication.java
@@ -0,0 +1,13 @@
+package com.mastertyres.master_tyres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterTyresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterTyresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastertyres/master_tyres/MasterTyresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastertyres.master_tyres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterTyresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
